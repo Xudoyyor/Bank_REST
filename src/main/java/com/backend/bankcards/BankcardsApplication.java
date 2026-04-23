b561b1acbe53a57fec518989dc84f0323dea5e05
Xudoyyor <hudoyordavronov4@gmail.com>
2026-04-23 17:05:24 +0500

    First code load

---
+++ src/main/java/com/backend/bankcards/BankcardsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.bankcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankcardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankcardsApplication.class, args);
+    }
+
+}
